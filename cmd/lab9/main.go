17441177c2e68f3ff6a876a7d4a8cceb21d1acc4
Dimtcho K. Dimov <ddimov@D-172-28-36-73.dhcp4.washington.edu>
2016-05-31 12:47:19 -0700

    fixed a bug

--- cmd/lab9/main.go
+++ cmd/lab9/main.go
@@ -137,7 +137,7 @@ func main() {
 		addr_line_1 := c.PostForm("addr_line_1")
 		addr_line_2 := c.PostForm("addr_line_2")
 		city := c.PostForm("city")
-		state_code := c.PostForm(state_code")
+		state_code := c.PostForm("state_code")
 
 
 		_, err := db.Exec("SELECT insert_person($1, $2, $3, $4, $5, $6, $7, $8);" , f_name, l_name, phone, email, addr_line_1, addr_line_2, city, state_code)
