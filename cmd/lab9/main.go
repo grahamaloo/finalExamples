1b2bc3bcabdeb8c7dfff5911797bfd4063e70bee
Dimtcho K. Dimov <ddimov@D-172-28-36-73.dhcp4.washington.edu>
2016-05-31 15:56:10 -0700

    adas

--- cmd/lab9/main.go
+++ cmd/lab9/main.go
@@ -191,10 +191,8 @@ func main() {
 			return
 		}
 		
-		current_time, err2 := time.Now().Local().Format("2006-01-02")
-		if err3 != nil {
-			c.AbortWithError(http.StatusInternalServerError, err3)
-		}
+		current_time := time.Now().Local()
+		f, err4 := strconv.ParseFloat(amount, 8)
 	
 		_, err = db.Exec("SELECT add_donation($1, $2, $3, $4)", amount, current_time.Format("2006-01-02"), personId, paymentId)
 		
