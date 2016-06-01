f88381dad7380e14cbebf3332959bdaeb8823488
Dimtcho K. Dimov <ddimov@D-172-28-36-70.dhcp4.washington.edu>
2016-05-31 20:02:53 -0700

    adadf

--- cmd/lab9/main.go
+++ cmd/lab9/main.go
@@ -81,8 +81,8 @@ func main() {
 	})
 
 	router.GET("/addresses", func(c *gin.Context) {
-		//rows, err := db.Query("SELECT first_line, second_line, city, state_code FROM address;")
-		rows, err := db.Query("WITH A AS (SELECT address_id AS event_addresses FROM event AS e NATURAL JOIN address) SELECT first_line, second_line, city, state_code FROM A.address;")
+		rows, err := db.Query("SELECT first_line, second_line, city, state_code FROM address;")
+		//rows, err := db.Query("WITH A AS (SELECT address_id AS event_addresses FROM event AS e NATURAL JOIN address) SELECT first_line, second_line, city, state_code FROM A.address;")
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 		}
