262ff05d9ba9e6ed91f7434a5230d47d940b030a
Dimtcho K. Dimov <ddimov@D-172-28-36-70.dhcp4.washington.edu>
2016-05-31 23:27:44 -0700

    s

--- cmd/lab9/main.go
+++ cmd/lab9/main.go
@@ -154,7 +154,7 @@ func main() {
 
 		current_time := time.Now().Local()
 		var paymentId int64
-		err = db.QueryRow("WITH A AS (INSERT INTO payment_method VALUES (DEFAULT) RETURNING payment_method.payment_method_id) INSERT INTO credit_card_payment(payment_method_id) VALUES ((SELECT payment_method_id FROM A), $1,$2 ) RETURNING credit_card_payment.payment_method_id;", card_num, card_exp).Scan(&paymentId)
+		err = db.QueryRow("WITH A AS (INSERT INTO payment_method VALUES (DEFAULT) RETURNING payment_method.payment_method_id) INSERT INTO credit_card_payment(payment_method_id, card_number, exp) VALUES ((SELECT payment_method_id FROM A), $1,$2) RETURNING credit_card_payment.payment_method_id;", card_num, card_exp).Scan(&paymentId)
 		_, err = db.Exec("SELECT add_donation($1, $2, $3, $4);" , amount, current_time.Format("2006-01-02"), personId, paymentId)
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
